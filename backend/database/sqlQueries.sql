62926d3b2471e00831f71f2593a1e41bb94e4a42
Osama Shabrez <osamashabrez@live.com>
2018-08-12 20:47:37 +0200

    Database Update for airport frontend

--- backend/database/sqlQueries.sql
+++ backend/database/sqlQueries.sql
@@ -16,10 +16,17 @@ CREATE TABLE `airports` (
   `country_id` INTEGER NOT NULL,
   `name` CHAR(100) NOT NULL UNIQUE,
   `IATA_code` CHAR(3) NOT NULL UNIQUE,
-  `longitude` FLOAT NOT NULL UNIQUE,
-  `latitude` FLOAT NOT NULL UNIQUE,
+  `longitude` FLOAT NOT NULL,
+  `latitude` FLOAT NOT NULL,
   FOREIGN KEY(country_id) REFERENCES countries(id)
 );
+INSERT INTO airports (country_id, name, IATA_code, longitude, latitude) VALUES (84, "Magical Airports 1", "MAP", 1.2223, 35.6666);
+INSERT INTO airports (country_id, name, IATA_code, longitude, latitude) VALUES (145, "Magical Airports 2", "MPA", 23.2223, 25.6666);
+INSERT INTO airports (country_id, name, IATA_code, longitude, latitude) VALUES (55, "Magical Airports 3", "MXP", 45.2223, 15.6666);
+INSERT INTO airports (country_id, name, IATA_code, longitude, latitude) VALUES (85, "Magical Airports 4", "XAP", 16.2223, 19.6666);
+INSERT INTO airports (country_id, name, IATA_code, longitude, latitude) VALUES (72, "Magical Airports 5", "MZP", 26.2223, 54.666);
+INSERT INTO airports (country_id, name, IATA_code, longitude, latitude) VALUES (94, "Magical Airports 6", "TAP", 12.2223, 95.6666);
+INSERT INTO airports (country_id, name, IATA_code, longitude, latitude) VALUES (184, "Magical Airports 7", "MQP", 67.2223, 55.6666);
 /* Run this query at the end, we need unique constraint on longitude and latitude as a set */
 ALTER TABLE `airports` ADD UNIQUE `unique_location`(`longitude`, `latitude`);
 
